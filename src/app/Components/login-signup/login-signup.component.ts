fcb2361e7cdb2b56824e0c10235a83b93f350a1f
aditya_kk <adityakrishnakumar12@gmail.com>
2022-03-15 19:11:39 +0530

    added swal alert boxes for errors

--- src/app/Components/login-signup/login-signup.component.ts
+++ src/app/Components/login-signup/login-signup.component.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+
 import { LoginSignupService } from 'src/app/services/login-signup.service';
+
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-login-signup',
@@ -11,6 +14,7 @@ export class LoginSignupComponent implements OnInit {
 
   isLogIn: boolean = true;
   isLoading: boolean = false;
+  errorMes : string = '';
 
   constructor(private loginSignupService: LoginSignupService) { }
 
@@ -34,8 +38,24 @@ export class LoginSignupComponent implements OnInit {
         console.log(res);
         this.isLoading = false;
       },
-        (error) => {
-          console.log(error);
+        (errorRes) => {
+          console.log(errorRes);
+          switch(errorRes.error.error.message){
+            case 'EMAIL_EXISTS' :
+              this.errorMes = "The email address is already in use by another account.";
+              break;
+            case 'INVALID_EMAIL':
+              this.errorMes = "The entered email is invalid.";
+              break;
+            case 'WEAK_PASSWORD : Password should be at least 6 characters':
+              this.errorMes = "The Password should be at least 6 characters.";
+              break;
+          }
+          Swal.fire({
+            icon: 'error',
+            title: 'Something went wrong!',
+            text: this.errorMes,
+          });
           this.isLoading = false;
         });
     }
